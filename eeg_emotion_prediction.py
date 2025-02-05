0c2b3a1aa2476dc23d5acd4d3efb17b58c0cea59
eunjju2 <graceun309@gmail.com>
2025-02-05 23:25:00 +0900

    Refactor : EEG 데이터 감정 분석 코드 개선

--- eeg_emotion_prediction.py
+++ eeg_emotion_prediction.py
@@ -9,225 +9,135 @@ import firebase_admin
 from firebase_admin import credentials, db, storage
 from uuid import uuid4
 from collections import Counter
+from dotenv import load_dotenv
 
+load_dotenv()
 
-cred = credentials.Certificate('emotion-palette-firebase-adminsdk-ns3sc-c1ffbdddc6.json')
+# Firebase 설정
+cred = credentials.Certificate(os.getenv('CREDENTIALS_PATH'))
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://emotion-palette-default-rtdb.firebaseio.com/',
-    'storageBucket': 'emotion-palette.appspot.com'})
+    'databaseURL': os.getenv('DATABASE_URL'),
+    'storageBucket': os.getenv('STORAGE_BUCKET')
+})
 
 bucket = storage.bucket()
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-id_name = "test" #ID 입력
-filereadname = 'D:/exhibition/' + id_name + ".csv"
-filename = id_name + ".csv"
+id_name = "test"  # ID 입력
+file_path = 'D:/exhibition/' + id_name + ".csv"
 
-
+# Firebase 업로드
 def fileUpload(file):
     blob = bucket.blob(file)
     new_token = uuid4()
     metadata = {"firebaseStorageDownloadTokens": new_token}
     blob.metadata = metadata
     blob.upload_from_filename(filename=file, content_type='csv')
-    print(blob.public_url)
+    return blob.public_url
 
-arr = []
+# 감정별 컬러 리스트
+color_lists = {
+    1: ['#f9f3d3', '#dfedb0', '#a5bc6c', '#c9bca9', '#b9c2a5'],
+    2: ['#8f8794', '#b299c3', '#cfaee7', '#c8b9be', '#988684'],
+    3: ['#d7be2d', '#833765', '#5d3b6e', '#db9148', '#c06793'],
+    4: ['#fdeded', '#eeced9', '#faeef2', '#f5eff9', '#cbb9b7'],
+    5: ['#c2448d', '#f59701', '#f5a6a2', '#f3d900', '#ffe76b'],
+    6: ['#cb003f', '#f3d900', '#01a46d', '#2c6fc0', '#007da3']
+}
 
+# 컬러 선택 
+def color_pick(emotion_num, count):
+    global color_list
+    if emotion_num in color_lists:
+        color_list.extend(color_lists[emotion_num][:count])
+        color_lists[emotion_num] = color_lists[emotion_num][count:]
+
+# 감정 매칭
+def get_emotion(num):
+    emotions = {1: "Natural", 2: "Elegant", 3: "Erotic",
+                4: "Romantic", 5: "Casual", 6: "Dynamic"}
+    return emotions.get(num, "Unknown")
+
+# 데이터 로드 및 전처리
+df = pd.read_csv(file_path).drop_duplicates(
+    ['delta'], keep='first').iloc[1:, 1:9].astype(int)
+signal_x = np.fft.fft(pdf1, axis=0) / len(df)
+df_scaled = StandardScaler().fit_transform(pd.DataFrame(abs(signal_x).astype(float),
+                                                        columns=['delta', 'theta', 'lowAlpha',
+                                                                 'highAlpha', 'lowBeta', 'highBeta',
+                                                                 'lowGamma', 'highGamma']))
+
+# 모델 예측
 new_model = keras.models.load_model('fft_model3.h5')
+predictions = new_model.predict(df_scaled)
 
-color_list1 = ['#f9f3d3', '#dfedb0', '#a5bc6c', '#c9bca9', '#b9c2a5']
-color_list2 = ['#8f8794', '#b299c3', '#cfaee7', '#c8b9be', '#988684']
-color_list3 = ['#d7be2d', '#833765', '#5d3b6e', '#db9148', '#c06793']
-color_list4 = ['#fdeded', '#eeced9', '#faeef2', '#f5eff9', '#cbb9b7']
-color_list5 = ['#c2448d', '#f59701', '#f5a6a2', '#f3d900', '#ffe76b']
-color_list6 = ['#cb003f', '#f3d900', '#01a46d', '#2c6fc0', '#007da3']
+# 예측된 감정 값 저장
+emotion_counts = Counter(np.argmax(predictions, axis=1) + 1) # axis=1 => predictions의 각 행에서 최댓값을 가진 인덱스
+top_emotions = emotion_counts.most_common(6)
+top3_emotions = top_emotions[:3]
 
+# 감정별 데이터 정리
+emotion_percentages = {
+    emotion: value / df_scaled.shape[0] * 100 for emotion, value in top_emotions}
+
+# 상위 3개 감정 비율 계산
+percents = sum(value for _, value in top3_emotions) / 10
+top3_percentages = {emotion: round(value / percents)
+                    for emotion, value in top3_emotions}
+
+
+total_top3 = sum(top3_percentages.values())
+
+# 10으로 맞추기
+if total_top3 > 10:
+    top3_percentages[top3_emotions[2][0]] -= (total_top3 - 10)
+elif total_top3 < 10:
+    top3_percentages[top3_emotions[2][0]] += (10 - total_top3)
+
+# 감정 최대 5로 제한
+if top3_percentages[top3_emotions[0][0]] > 5:
+    excess = top3_percentages[top3_emotions[0][0]] - 5
+    top3_percentages[top3_emotions[0][0]] = 5
+    top3_percentages[top3_emotions[1][0]] += excess
+
+if top3_percentages[top3_emotions[1][0]] > 5:
+    excess = top3_percentages[top3_emotions[1][0]] - 5
+    top3_percentages[top3_emotions[1][0]] = 5
+    top3_percentages[top3_emotions[2][0]] += excess
+
+# 컬러 리스트 생성
 color_list = []
+for emotion, value in top3_percentages.items():
+    half_count = math.ceil(value / 2)
+    color_pick(emotion, half_count)
+    color_pick(emotion, count - half_count)
+
+emotion_all = [(get_emotion(emotion), round(percentage, 2))
+               for emotion, percentage in emotion_percentages.items()]
+
+emotion_top3_color = [(get_emotion(emotion), value)
+                      for emotion, value in top3_percentages.items()],
 
 
-def color_pick1(n):
-    for i in range(n):
-        color_list.append(color_list1[i])
-    del color_list1[0:n]
+fileUpload(file_path)
+
+db.reference().update({id_name: {'emotion_all': emotion_all,
+                                 'emotion_top3_color': emotion_top3_color, 'colorlist': color_list}})
 
 
-def color_pick2(n):
-    for i in range(n):
-        color_list.append(color_list2[i])
-    del color_list2[0:n]
+print("\n감정 결과 (Top 3):", [get_emotion(emotion)
+      for emotion, _ in top3_emotions])
 
+print("\n상위 3가지 감정 유사율:")
+for emotion, percentage in emotion_percentages.items():
+    print(f"[ {get_emotion(emotion)} : {percentage:.2f}% ]")
 
-def color_pick3(n):
-    for i in range(n):
-        color_list.append(color_list3[i])
-    del color_list3[0:n]
+print("\n첫 번째 감정, 컬러칩 개수:", get_emotion(
+    top3_emotions[0][0]), top3_percentages[top3_emotions[0][0]])
+print("두 번째 감정, 컬러칩 개수:", get_emotion(
+    top3_emotions[1][0]), top3_percentages[top3_emotions[1][0]])
+print("세 번째 감정, 컬러칩 개수:", get_emotion(
+    top3_emotions[2][0]), top3_percentages[top3_emotions[2][0]])
 
-
-def color_pick4(n):
-    for i in range(n):
-        color_list.append(color_list4[i])
-    del color_list4[0:n]
-
-
-def color_pick5(n):
-    for i in range(n):
-        color_list.append(color_list5[i])
-    del color_list5[0:n]
-
-
-def color_pick6(n):
-    for i in range(n):
-        color_list.append(color_list6[i])
-    del color_list6[0:n]
-
-
-def aaa(num, c):
-    if num == 1:
-        color_pick1(c)
-    elif num == 2:
-        color_pick2(c)
-    elif num == 3:
-        color_pick3(c)
-    elif num == 4:
-        color_pick4(c)
-    elif num == 5:
-        color_pick5(c)
-    elif num == 6:
-        color_pick6(c)
-
-
-def emotion(num):
-    if num == 1:
-        return "Natural"
-    elif num == 2:
-        return "Elegant"
-    elif num == 3:
-        return "Erotic"
-    elif num == 4:
-        return "Romantic"
-    elif num == 5:
-        return "Casual"
-    elif num == 6:
-        return "Dynamic"
-
-
-pre_df = pd.read_csv(filereadname)
-pdf1 = pre_df.drop_duplicates(['delta'], keep='first')
-pdf1 = pdf1.drop(index=0)
-pdf1 = pdf1.iloc[:, 1:9].astype(int)  # numarr
-
-signal_x = np.fft.fft(pdf1, axis=0) / len(pdf1)
-df1_d = pd.DataFrame(abs(signal_x).astype(float),
-                     columns=['delta', 'theta', 'lowAlpha',
-                              'highAlpha', 'lowBeta', 'highBeta',
-                              'lowGamma', 'highGamma'])
-
-df_sc = StandardScaler().fit_transform(df1_d)
-print(df_sc.shape)
-predictions = new_model.predict(df_sc)
-for i in range(df_sc.shape[0]):
-    arr.append(np.argmax(predictions[i]) + 1)
-
-count_items = Counter(arr)
-count_items_top = count_items.most_common(n=6)
-
-
-print("-------------------")
-print(arr)
-print(count_items)
-print("-------------------")
-
-first = count_items_top[0][0]  # 감정
-second = count_items_top[1][0]
-third = count_items_top[2][0]
-fourth = count_items_top[3][0]
-fifth = count_items_top[4][0]
-sixth = count_items_top[5][0]
-
-first_var = count_items_top[0][1]  # 값
-second_var = count_items_top[1][1]
-third_var = count_items_top[2][1]
-fourth_var = count_items_top[3][1]
-fifth_var = count_items_top[4][1]
-sixth_var = count_items_top[5][1]
-
-first_rate = first_var / df_sc.shape[0] * 100  # 유사율
-second_rate = second_var / df_sc.shape[0] * 100
-third_rate = third_var / df_sc.shape[0] * 100
-fourth_rate = fourth_var / df_sc.shape[0] * 100
-fifth_rate = fifth_var / df_sc.shape[0] * 100
-sixth_rate = sixth_var / df_sc.shape[0] * 100
-
-percent = (first_var + second_var + third_var) / 10
-percent2 = (first_var + second_var + third_var) / 100
-
-first_per = round(first_var / percent)
-second_per = round(second_var / percent)
-third_per = round(third_var / percent)
-
-
-all = first_per + second_per + third_per
-if all > 10:
-    third_per = third_per - (all-10)
-
-if first_per >= 5:
-    first_per = 5
-    second_per += first_per - 5
-    if second_per > 5:
-        third_per += second_per - 5
-
-if all < 10 :
-    third_per = third_per + (10-all)
-
-
-first_cnt = math.ceil(first_per / 2)
-second_cnt = math.ceil(second_per / 2)
-third_cnt = math.floor(third_per / 2)
-
-aaa(first, first_cnt)
-aaa(second, second_cnt)
-aaa(third, third_cnt)
-aaa(first, first_per - first_cnt)
-aaa(second, second_per - second_cnt)
-aaa(third, third_per - third_cnt)
-
-emotion_all = [(emotion(first), (first_var / first_var) * 100),
-               (emotion(second), (second_var / first_var) * 100),
-               (emotion(third), (third_var / first_var) * 100),
-               (emotion(fourth), (fourth_var / first_var) * 100),
-               (emotion(fifth), (fifth_var / first_var) * 100),
-               (emotion(sixth), (sixth_var / first_var) * 100)]
-
-emotion_top3 = [emotion(first), emotion(second), emotion(third)]
-
-emotion_top3_rate = [[emotion(first), (first_var / percent2)],
-                     [emotion(second), (second_var / percent2)],
-                     [emotion(third), (third_var / percent2)]]
-
-emotion_top3_color = [(emotion(first), first_per),
-                      (emotion(second), second_per),
-                      (emotion(third), third_per)]
-
-                                                                             emotion(fourth),round((fourth_var / first_var)*10),emotion(fifth),round((fifth_var / first_var)*10),emotion(sixth),round((sixth_var / first_var)*10)))
-print("[ %s : %f ]\n[ %s : %f ]\n[ %s : %f ]\n[ %s : %f ]\n[ %s : %f ]\n[ %s : %f ] \n" % (
-emotion(first), (first_var / first_var) * 100, emotion(second), (second_var / first_var) * 100, emotion(third),
-(third_var / first_var) * 100,
-emotion(fourth), (fourth_var / first_var) * 100, emotion(fifth), (fifth_var / first_var) * 100, emotion(sixth),
-(sixth_var / first_var) * 100))
-
-print("감정 결과 : '%s' , '%s' , '%s' \n" % (emotion(first), emotion(second), emotion(third)))
-
-print('상위 3가지 감정 유사율 :\n[ %s : %f ]\n[ %s : %f ]\n[ %s : %f ] \n' % (
-emotion(first), (first_var / percent2), emotion(second), (second_var / percent2), emotion(third),
-(third_var / percent2)))
-print('첫번째 감정 , 컬러칩 개수 : %s , %d \n두번째 감정 , 컬러칩 개수 : %s , %d \n세번째 감정 , 컬러칩 개수 : %s , %d \n ' % (
-emotion(first), first_per, emotion(second), second_per, emotion(third), third_per))
-
-
-print(color_list)
-fileUpload(filereadname)
-
-ref = db.reference()
-ref.update({id_name: {'emotion_all': emotion_all, 'emotion_top3_color': emotion_top3_color, 'colorlist': color_list}})
+print("\n컬러 리스트:", color_list)
