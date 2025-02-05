4f73fc57e72655b22da72a1e5255c7990b35c551
eunjju2 <graceun309@gmail.com>
2025-02-05 23:53:54 +0900

    Refactor : EEG 데이터 딥러닝 모델 학습 코드 개선

--- eeg_model.py
+++ eeg_model.py
@@ -1,5 +1,5 @@
-import numpy as np 
-import pandas as pd  
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import tensorflow as tf
@@ -27,15 +27,18 @@ df = pd.read_csv('dataset0913.csv')
 
 y = pd.get_dummies(df['label'])
 
-top_fea = ['delta', 'theta', 'lowAlpha', 'highAlpha','lowBeta', 'highBeta', 'lowGamma', 'highGamma']
+features = ['delta', 'theta', 'lowAlpha', 'highAlpha',
+            'lowBeta', 'highBeta', 'lowGamma', 'highGamma']
 
-df_sc = StandardScaler().fit_transform(df[top_fea])
+df_scaled = StandardScaler().fit_transform(df[features])
 
-Xtr, xte, Ytr, yte = train_test_split(df_sc, y,  test_size=0.1)
-xtr, xval, ytr, yval = train_test_split(Xtr, Ytr,  test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(
+    df_scaled, y,  test_size=0.1)
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train,  test_size=0.1)
 
 model = keras.Sequential([
-    layers.Dense(128, input_dim=(len(df_sc[0])), activation='relu'),
+    layers.Dense(128, input_dim=(len(df_scaled[0])), activation='relu'),
     layers.Dense(256, activation='relu'),
     layers.Dense(512, activation='relu'),
     layers.Dropout(0.1),
@@ -46,30 +49,32 @@ model = keras.Sequential([
 ])
 
 # Compiling the model with Adamax Optimizer
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-call = callbacks.EarlyStopping(patience=20, min_delta=0.0001, restore_best_weights=True)
+early_stopping = callbacks.EarlyStopping(
+    patience=20, min_delta=0.0001, restore_best_weights=True)
 
-history = model.fit(Xtr, Ytr, validation_data=(xval,yval), batch_size=40, epochs=250, callbacks=[call])
+history = model.fit(x_train, y_train, validation_data=(x_val, y_val),
+                    batch_size=40, epochs=250, callbacks=[early_stopping])
 
-result= model.evaluate(xte,yte)
-print("\n정확도 :  %.4f \n\n\n\n" %(result[1] * 100))
+result = model.evaluate(x_test, y_test)
+print("\n정확도 :  %.4f \n\n\n\n" % (result[1] * 100))
 
 training = pd.DataFrame(history.history)
 training.loc[:, ['loss', 'val_loss']].plot()
 training.loc[:, ['accuracy', 'val_accuracy']].plot()
 
-
-xhat_idx = np.random.choice(xte.shape[0], 30)
-xhat = xte[xhat_idx]
+# 예측
+xhat_idx = np.random.choice(x_test.shape[0], 30)
+xhat = x_test[xhat_idx]
 yhat = model.predict(xhat)
 for i in range(30):
-    print('True : ' + str(np.argmax(yte.iloc[xhat_idx[i]])) + ', Predict : ' + str(np.argmax(yhat[i])))
+    print('True : ' + str(np.argmax(y_test.iloc[xhat_idx[i]])
+                          ) + ', Predict : ' + str(np.argmax(yhat[i])))
 
 
 model.save('fft_model3.h5')
 
-
 plot_model(model, to_file='model.png', show_shapes=True, )
-
 plt.show()
